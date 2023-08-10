5443fe6266d073092659230327bb15c5af438c04
Kaveri Priya Putti <kaveripriyap2312@gmail.com>
2023-08-10 21:53:49 +0800

    Update appListExtractScript.js

--- appListExtractScript.js
+++ appListExtractScript.js
@@ -4,21 +4,17 @@ const csv = require('csv-parser');
 const inputFile = 'path_to_your_input_csv_file.csv'; // Update with the path to your CSV file
 const outputFile = 'path_to_your_output_json_file.json'; // Update with the desired output JSON file path
 
+const columnHeaders = ['name', 'asCode', 'overallStatus', 'mcStatus', 'appdStatus', 'guiStatus'];
 const transformedData = [];
 
 fs.createReadStream(inputFile)
-  .pipe(csv())
+  .pipe(csv({ separator: ';' })) // Specify the correct separator used in your CSV file
   .on('data', (row) => {
-    if (row[3] && row[4]) { // Check if columns 3 and 4 are not empty
-      transformedData.push({
-        name: row[3],
-        asCode: row[4],
-        overallStatus: '',
-        mcStatus: '',
-        appdStatus: '',
-        guiStatus: '',
-      });
+    const entry = {};
+    for (let i = 0; i < columnHeaders.length; i++) {
+      entry[columnHeaders[i]] = row[i];
     }
+    transformedData.push(entry);
   })
   .on('end', () => {
     fs.writeFileSync(outputFile, JSON.stringify(transformedData, null, 2));
